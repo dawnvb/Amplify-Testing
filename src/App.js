bca4aca8443beab30215d9cf48f1eaef5dc980de
Ratnasambhav Priyadarshi <ratnasam@amazon.com>
2020-09-14 14:20:50 +0530

    Added auth and tests for auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { withAuthenticator, SignOut } from 'aws-amplify-react'
+import Amplify from 'aws-amplify';
+
+import aws_exports from './aws-exports';
 import './App.css';
+
+Amplify.configure(aws_exports);
 
 function App() {
   return (
     <div className="App">
+      <SignOut/>
       <header className="App-header">
         Amplify Testing
       </header>
@@ -12,4 +18,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
